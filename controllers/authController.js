2d87a902ac966cbcf19c65f29e512168da41ceff
Serj <sek.sazonov@gmail.com>
2022-04-06 13:21:39 +0300

    rework payload

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,47 +8,31 @@ const bcrypt = require('bcryptjs');
 
 module.exports = {
     async registration(req, res) {
+        const role = 'USER';
         const { email, password, name } = req.body;
-        const { data: { id, error } } = await userService.registration({ email, password, name })
+        const { data: { id, error } } = await userService.registration({ email, password, name })        
 
         if (error) {
             return res.status(400).json(error);
         }
-
+        
         const hashPassword = bcrypt.hashSync(password, 7);
+        const candidate = await userRepository.getByEmail(email);
+        if (candidate) {
+            return res.status(400).json({ message: 'user with this email already exist'})
+        }
+        
 
         await userRepository.saveUser(id, email, hashPassword);
 
-        const { accessToken, refreshToken } = tokenService.createToken({ id });
+        const { accessToken, refreshToken } = tokenService.createToken({ id, role });
 
-        res.cookie('token', refreshToken, { httpOnly: true });
-        await authRepository.clearUserTokens(id);
-        await authRepository.saveUserToken(id, refreshToken, role = 'USER');
-        res.status(200).send({ accessToken, refreshToken });
-    },
-
-    async policeRegistration(req, res) {
-        const { email, password } = req.body;
-        const { data: { id, error } } = await policeService.registration({ email, password });
-
-        if (error) {
-            return res.status(400).json(error);
-        }
-
-        const hashPassword = bcrypt.hashSync(password, 7);
-
-        await userRepository.saveUser(id, email, hashPassword, 'POLICE');
-
-        const { accessToken, refreshToken } = tokenService.createToken({ id });
-
-        res.cookie('token', refreshToken, { httpOnly: true });
-        await authRepository.clearUserTokens(id);
-        await authRepository.saveUserToken(id, refreshToken, role = 'POLICE');
+        res.cookie('token', refreshToken, { httpOnly: true });        
+        await authRepository.saveUserToken(id, refreshToken, role);
         res.status(200).send({ accessToken, refreshToken });
     },
 
     getUsers(req, res) {
-        
         authRepository
             .getAll(req.query)
             .then((result) => res.status(200).json(result))
@@ -67,7 +51,7 @@ module.exports = {
         if (!resultPass) {
             return res.status(400).json("OPS WRONG PASSWORD");
         }
-        const { accessToken, refreshToken } = tokenService.createToken({ id: user.userid });
+        const { accessToken, refreshToken } = tokenService.createToken({ id: user.userid, role: user.role });
 
         await authRepository.clearUserTokens(user.userid);
         await authRepository.saveUserToken(user.userid, refreshToken, role = user.role);
@@ -75,7 +59,6 @@ module.exports = {
     },
 
     async logout(req, res) {
-
         const authHeader = req.headers.authorization;
         const data = await jwt.verify(authHeader.split(' ')[1], secret.refreshKey);
 
@@ -86,20 +69,16 @@ module.exports = {
     async refresh(req, res) {
         let cookieToken = req.headers.cookie
         let result = cookieToken.replace(/^.{6}/, '');
-
         const item = await authRepository.getByToken(result);
 
         if (!item) {
             return res.status(404).json({ message: 'not auth' });
         }
 
-        const { id } = item;
-
-        const { accessToken, refreshToken } = tokenService.createToken({ id });
+        const { accessToken, refreshToken } = tokenService.createToken({ id: item.id, role: item.role });
 
         await authRepository.clearUserTokens(item.id);
-
-        await authRepository.saveUserToken(item.id, refreshToken, role = 'USER');
+        await authRepository.saveUserToken(item.id, refreshToken, item.role);
         res.status(200).send({ accessToken, refreshToken });
     }
 }
